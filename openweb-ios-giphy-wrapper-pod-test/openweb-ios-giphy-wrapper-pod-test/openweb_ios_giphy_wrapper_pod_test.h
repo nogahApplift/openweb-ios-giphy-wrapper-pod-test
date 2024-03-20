665b81d76f84ae4112ffa2849636187643d06149
Nogah <nogah@applift-consulting.com>
2024-03-20 17:13:05 +0200

    0.1.3 - _exported

---
+++ openweb-ios-giphy-wrapper-pod-test/openweb-ios-giphy-wrapper-pod-test/openweb_ios_giphy_wrapper_pod_test.h
@@ -0,0 +1,18 @@
+//
+//  openweb_ios_giphy_wrapper_pod_test.h
+//  openweb-ios-giphy-wrapper-pod-test
+//
+//  Created by  Nogah Melamed on 20/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for openweb_ios_giphy_wrapper_pod_test.
+FOUNDATION_EXPORT double openweb_ios_giphy_wrapper_pod_testVersionNumber;
+
+//! Project version string for openweb_ios_giphy_wrapper_pod_test.
+FOUNDATION_EXPORT const unsigned char openweb_ios_giphy_wrapper_pod_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openweb_ios_giphy_wrapper_pod_test/PublicHeader.h>
+
+
